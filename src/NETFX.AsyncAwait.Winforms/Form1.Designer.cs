504188aacfe7a468b120d8b3ee8cbc298e38d5f9
eriawan <eriawan@live.com>
2024-05-22 04:51:53 +0700

    Ensure async is enclosed in a method

--- src/NETFX.AsyncAwait.Winforms/Form1.Designer.cs
+++ src/NETFX.AsyncAwait.Winforms/Form1.Designer.cs
@@ -33,7 +33,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnGetWikiSync = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTimerSync = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnGetWikiAsync = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
@@ -66,6 +66,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(18, 57);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
@@ -81,10 +82,11 @@
             this.btnGetWikiSync.TabIndex = 1;
             this.btnGetWikiSync.Text = "Get Wikioedia article of Visual Studio Synchronous";
             this.btnGetWikiSync.UseVisualStyleBackColor = true;
+            this.btnGetWikiSync.Click += new System.EventHandler(this.btnGetWikiSync_Click);
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.lblTimerSync);
             this.groupBox3.Location = new System.Drawing.Point(477, 25);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(251, 114);
@@ -92,18 +94,19 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Timer synch";
             // 
-            // label1
+            // lblTimerSync
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(20, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 33);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "00:00:00";
+            this.lblTimerSync.AutoSize = true;
+            this.lblTimerSync.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimerSync.Location = new System.Drawing.Point(20, 32);
+            this.lblTimerSync.Name = "lblTimerSync";
+            this.lblTimerSync.Size = new System.Drawing.Size(131, 39);
+            this.lblTimerSync.TabIndex = 0;
+            this.lblTimerSync.Text = "00:00:00";
             // 
             // textBox2
             // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(18, 62);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
@@ -117,8 +120,9 @@
             this.btnGetWikiAsync.Name = "btnGetWikiAsync";
             this.btnGetWikiAsync.Size = new System.Drawing.Size(349, 23);
             this.btnGetWikiAsync.TabIndex = 3;
-            this.btnGetWikiAsync.Text = "Get Wikioedia article of Visual Studio Synchronous";
+            this.btnGetWikiAsync.Text = "Get Wikioedia article of Visual Studio Asynchronous";
             this.btnGetWikiAsync.UseVisualStyleBackColor = true;
+            this.btnGetWikiAsync.Click += new System.EventHandler(this.btnGetWikiAsync_Click);
             // 
             // Form1
             // 
@@ -130,6 +134,7 @@
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "C# Async await ";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -145,7 +150,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTimerSync;
         private System.Windows.Forms.Button btnGetWikiSync;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnGetWikiAsync;
--- src/NETFX.AsyncAwait.Winforms/Form1.cs
+++ src/NETFX.AsyncAwait.Winforms/Form1.cs
@@ -4,17 +4,83 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace NETFX.AsyncAwait.Winforms
 {
     public partial class Form1 : Form
     {
+        private const string VISUALSTUDIO_WIKI_URL = "https://en.wikipedia.org/wiki/Visual_Studio";
+        private System.Timers.Timer timerSync = new System.Timers.Timer();
+
         public Form1()
         {
             InitializeComponent();
+            timerSync.Interval = 500;
+            timerSync.Elapsed += TimerSync_Elapsed;
+            timerSync.Start();
+        }
+
+        private void btnGetWikiSync_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(2000);
+            using (HttpClient client = new HttpClient())
+            {
+                string wikipediaContent = "";
+                for (int i = 0; i < 10; i++)
+                {
+                    wikipediaContent = client.GetStringAsync(VISUALSTUDIO_WIKI_URL).Result;
+                    Thread.Sleep(200);
+                }
+                textBox1.Text = wikipediaContent;
+            }
+        }
+
+        private void TimerSync_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (lblTimerSync.InvokeRequired)
+            {
+                lblTimerSync.Invoke(new MethodInvoker(delegate
+                {
+                    lblTimerSync.Text = DateTime.Now.ToString("HH:mm:ss");
+                }));
+            }
+            else
+            {
+                lblTimerSync.Text = DateTime.Now.ToString("HH:mm:ss");
+            }
+        }
+
+        private async void btnGetWikiAsync_Click(object sender, EventArgs e)
+        {
+            await Task.Delay(2000);
+            string wikipediaContent = "";
+            wikipediaContent = await GetWikiVisualStudioAsync();
+            textBox2.Text = wikipediaContent;
+        }
+
+        private async Task<string> GetWikiVisualStudioAsync()
+        {
+            string wikipediaContent = "";
+            using (HttpClient client = new HttpClient())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    wikipediaContent = await client.GetStringAsync(VISUALSTUDIO_WIKI_URL);
+                    await Task.Delay(200);
+                }
+            }
+            return wikipediaContent;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
